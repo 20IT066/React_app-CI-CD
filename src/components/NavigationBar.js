40d3861721d10b7bfcf1c228ba26f24049f806ec
Parth Mangukiya <20it066@charusat.edu.in>
2023-04-10 14:25:42 +0530

    Final test from parth

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -12,7 +12,7 @@ export default function NavigationBar() {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
 
-        <Link to="/" className='navbar-brand'>Student Management System by Parth and jay</Link>
+        <Link to="/" className='navbar-brand'>Student Management System by 20IT066</Link>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
